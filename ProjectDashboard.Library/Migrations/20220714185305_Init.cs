4bd83375b4d88cd2e5108d55081c26fa7a80d532
Turbék György <gyorgy.turbek@gmail.com>
2022-07-14 20:54:42 +0200

    Added ef core migration.

---
+++ ProjectDashboard.Library/Migrations/20220714185305_Init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectDashboard.Library.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Standalones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PublishPath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DebugPath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleasePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SolutionPath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Standalones", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Standalones");
+        }
+    }
+}
