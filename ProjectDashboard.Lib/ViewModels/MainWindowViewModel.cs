59c3b91340dc1b6899850b7ca2cfd538ac995e00
György Turbék <profgyuri@gmail.com>
2022-12-26 20:24:38 +0100

    Add project files.

---
+++ ProjectDashboard.Lib/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectDashboard.Lib.ViewModels;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+[INotifyPropertyChanged]
+public partial class MainWindowViewModel
+{
+    [ObservableProperty] private ObservableCollection<string> projectNames;
+
+	public MainWindowViewModel()
+	{
+		projectNames = new()
+		{
+			"Multitasking",
+			"Music Listening",
+			"Something, that is hopefully too long for 2 lines, so the text will be trimmed."
+		};
+	}
+}
---
+++ ProjectDashboard.WPF/Resources/CorneredButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ProjectDashboard.WPF.Resources;
+
+public sealed class CorneredButton : Button
+{
+    #region Border Radius Property
+    public static readonly DependencyProperty BorderRadiusProperty =
+        DependencyProperty.Register("BorderRadius", typeof(CornerRadius), typeof(CorneredButton), new PropertyMetadata(default(CornerRadius)));
+
+    public CornerRadius BorderRadius
+    {
+        get { return (CornerRadius)GetValue(BorderRadiusProperty); }
+        set { SetValue(BorderRadiusProperty, value); }
+    }
+    #endregion
+
+    #region Mouse Over Background Property
+    public static readonly DependencyProperty MouseOverBackgroundProperty =
+        DependencyProperty.Register(
+            "MouseOverBackground",
+            typeof(Brush),
+            typeof(CorneredButton),
+            new PropertyMetadata(Brushes.Transparent));
+
+    public Brush MouseOverBackground
+    {
+        get { return (Brush)GetValue(MouseOverBackgroundProperty); }
+        set { SetValue(MouseOverBackgroundProperty, value); }
+    }
+    #endregion
+
+    #region Pressed Background Property
+    public static readonly DependencyProperty PressedBackgroundProperty =
+        DependencyProperty.Register(
+            "PressedBackground",
+            typeof(Brush),
+            typeof(CorneredButton),
+            new PropertyMetadata(Brushes.Transparent));
+
+    public Brush PressedBackground
+    {
+        get { return (Brush)GetValue(PressedBackgroundProperty); }
+        set { SetValue(PressedBackgroundProperty, value); }
+    }
+    #endregion
+}
