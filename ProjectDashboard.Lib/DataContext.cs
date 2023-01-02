b9ad06f6b1cc938d55ca4f645f9f727c73822cee
György Turbék <profgyuri@gmail.com>
2023-01-02 03:08:12 +0100

    Added ef core datacontext

---
+++ ProjectDashboard.Lib/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectDashboard.Lib.Models;
+
+namespace ProjectDashboard.Lib;
+
+public class DataContext : DbContext
+{
+    public DbSet<Solution> Solutions { get; set; }
+
+    #region Overrides of DbContext
+    /// <inheritdoc />
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlite("Data Source=dashboard.db;");
+    }
+    #endregion
+}
